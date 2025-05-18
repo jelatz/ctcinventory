a4e5778bbaaf2c7e7093d68e1deb8e4f49c11f3f
Jelatz <lanetejlad@gmail.com>
2025-05-18 12:33:08 +0800

    added forminput for the items page

--- resources/js/pages/Item/Index.vue
+++ resources/js/pages/Item/Index.vue
@@ -7,13 +7,37 @@
                     @click="itemModal = true">
                     Add Item
                 </button>
-                <!-- User Modal -->
-                <Modal :isOpen="itemModal" @close="itemModal = false" modalSize="w-1/2">
+                <!-- Item Modal -->
+                <Modal :isOpen="itemModal" @close="itemModal = false" modalSize="w-3/4">
                     <h2 class="text-xl font-semibold mb-4">Add Item</h2>
                     <form>
-                        <FormInput formLabel="Item Name" labelFor="item" formInputType="input" v-model="item"
-                            inputClass="w-full" :formError="false" errorMessage="Please enter a brand"
-                            containerClass="w-full" />
+                        <div class="flex items-center justify-between gap-5 flex-wrap mt-10">
+                            <FormInput formLabel="Item Name" labelFor="item" formInputType="input" v-model="item"
+                                inputClass="w-full" :formError="false" errorMessage="Please enter a brand"
+                                containerClass="w-60" />
+                            <FormInput formLabel="Item Code" labelFor="itemCode" formInputType="input"
+                                v-model="itemCode" inputClass="w-full" :formError="false"
+                                errorMessage="Please enter a brand" containerClass="w-60" />
+                            <FormInput formLabel="Serial Number" labelFor="serialNumber" formInputType="input"
+                                inputType="text" v-model="serialNumber" inputClass="w-full" :formError="false"
+                                errorMessage="This field is required" containerClass="w-60" />
+                            <FormInput formLabel="Model" labelFor="model" formInputType="input" inputType="text"
+                                v-model="model" inputClass="w-full" :formError="false"
+                                errorMessage="This field is required" containerClass="w-60" />
+                            <FormInput formLabel="Warranty Due" labelFor="warrantyDue" formInputType="input"
+                                inputType="text" v-model="warrantyDue" inputClass="w-full" :formError="false"
+                                errorMessage="This field is required" containerClass="w-60" />
+                            <FormInput formLabel="Shelf Life" labelFor="shelfLife" formInputType="input"
+                                inputType="text" v-model="shelfLife" inputClass="w-full" :formError="false"
+                                errorMessage="This field is required" containerClass="w-60" />
+                            <FormInput formLabel="Brand" labelFor="brand" formInputType="input" inputType="text"
+                                v-model="brand" inputClass="w-full" :formError="false"
+                                errorMessage="This field is required" containerClass="w-60" />
+                            <FormInput formLabel="Supplier" labelFor="supplier" formInputType="input" inputType="text"
+                                v-model="supplier" inputClass="w-full" :formError="false"
+                                errorMessage="This field is required" containerClass="w-60" />
+                        </div>
+
                         <button type="submit"
                             class="mt-5 px-10 py-2 rounded-lg bg-blue-950 text-white cursor-pointer hover:bg-blue-900 block ml-auto">
                             Add
@@ -70,6 +94,13 @@ onMounted(() => {
     currentHeaders.value = tableSets.totalItems.headers;
     currentRows.value = tableSets.totalItems.rows;
 });
+
+// hide modal on escape key press
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        itemModal.value = false;
+    }
+})
 </script>
 
 <style scoped></style>
